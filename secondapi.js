1d8cb50e5f19a5b3003ebbf0984002078ac84b2f
candrepa1 <candrepa1@gmail.com>
2021-01-15 19:41:43 -0500

    all corrections made

--- secondapi.js
+++ secondapi.js
@@ -1,9 +1,11 @@
+// Calls to the API to get the list of categories 
 function getCategories() {
     fetch('https://opentdb.com/api_category.php')
         .then((response) => response.json())
         .then((data) => selectCategories(data))
 }
 
+// Gets the categories' options from the previous API call and adds each category to an option element
 function selectCategories(data) {
     let content = document.getElementById('category');
     
@@ -12,6 +14,7 @@ function selectCategories(data) {
     });
 }
 
+// Gets the questions when the user clicks on the button, calls the API based on the number of questions, category, level of difficulty, and type of question chosen. 
 function getQuestions() {
     let numberOfQuestions = document.getElementById('questions').value;
     let categorySelected = document.getElementById('category').value;
@@ -25,6 +28,7 @@ function getQuestions() {
         .then((info) => printQuestions(info))
 }
 
+// The function is called in the API call, prints each question available depending on the parameters chosen by the user. Also gets the answers of each question, both correct and incorrect, and the prints them by calling getAnswersHTML().
 function printQuestions(info) {
     let content = document.getElementById('questions-container');
 
@@ -49,9 +53,10 @@ function printQuestions(info) {
                     </div>
                 </div>`;
     });
-    content.innerHTML += `<input type="submit" class="btn btn-primary mb-3 mw-90" value="Submit Answers">`;
+    content.innerHTML += `<div class="d-flex justify-content-center"><input type="submit" class="btn btn-primary py-2 px-5 mb-3" value="Submit Answers"></div>`;
 }
 
+// Prints the answers for the questions, checks if the current answer inside the loop is the correct one, if it is, it adds 'correct' as its value, if not, it adds 'incorrect' to its value. 
 function getAnswersHTML(randomAnswers, index, cAnswer) {
 
     let result = '';
@@ -75,6 +80,7 @@ function getAnswersHTML(randomAnswers, index, cAnswer) {
     return result;
 }
 
+// Upon form submittion. Gets the value of the selections made by the user. Loops through all the selections and checks if the answer selected is the correct answer, if it is, adds 1 to the counter. 
 function validatingAnswers() {
     let counter = 0;
     let length = document.getElementById('questions').value;
@@ -85,17 +91,17 @@ function validatingAnswers() {
             counter += 1;
         }
     }
-    printScore(counter);
+    printScore(counter, length);
 }
 
-function printScore(counter) {
+// Receives counter and length from validatingAnswers(), adds the innerHTML for the score portion, it uses the counter to keep track of the right answers and length to know the number of questions being asked. 
+function printScore(counter, length) {
     let content = document.getElementById('score-container');
-    let numberQ = document.getElementById('questions').value;
 
     content.innerHTML = `<div class="card border-success mb-3 mx-auto" style="max-width: 18rem;">
         <div class="card-body text-success">
-        <h5 class="card-title">Your Score: ${counter}/${numberQ}</h5>
-        <p class="card-text">Correct answers: ${counter} <br> Wrong answers: ${numberQ - counter} <br> Total answers: ${numberQ}</p>
+        <h5 class="card-title">Your Score: ${counter}/${length}</h5>
+        <p class="card-text">Correct answers: ${counter} <br> Wrong answers: ${length - counter} <br> Total answers: ${length}</p>
         </div>
     </div>`
 }
